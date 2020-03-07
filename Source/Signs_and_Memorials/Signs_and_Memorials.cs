129d6855a6664803e9cd69d94e9d0b43be3f46c3
jtgibson01 <jtgibson@telus.net>
2020-03-06 21:43:41 -0800

    Code patch
    * Rename SaM.Base to SaM.Building_Memorial_Base
    * Re-add (working) Mod Settings, fix crash on load
    * Auto-open setting
    * Add pause-game-on-edit setting
    * Save static reference to mod settings in SaM_Mod instead of per Thing
    * Read/write text in base-64 rather than dumping raw user text to file
    * Disable inspect string hijinks (B18+ removed line limit on inspect panel)

--- Source/Signs_and_Memorials/Signs_and_Memorials.cs
+++ Source/Signs_and_Memorials/Signs_and_Memorials.cs
@@ -4,22 +4,76 @@ using System.Collections.Generic;
 using Verse;
 using RimWorld;
 using UnityEngine;
+using System.Text;
 
 namespace SaM {
 
 	public class SaM_Mod : Verse.Mod {
+		public static SaM_ModSettings settings;
+		
+		//
+		// Constructors
+		//
+		public SaM_Mod(Verse.ModContentPack content) : base(content) {
+			settings = this.GetSettings<SaM_ModSettings>();
+			}
+		
+		//
+		// Methods
+		//
+		public override String SettingsCategory() {
+			return "SaM_Settings_category".Translate();
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			Text.Font = GameFont.Small;
+
+			float margin = 10;
+			float xPos = inRect.x + margin;
+			float yOff = inRect.y + margin;
+			float posW = inRect.width - 2 * margin;
+
+			Rect editCheckbox = new Rect(xPos, yOff, posW, 24);
+			yOff += editCheckbox.height + 1;
+			Rect editDescription = new Rect(xPos, yOff, posW, 24);
+
+			Widgets.CheckboxLabeled(editCheckbox, "SaM_Settings_editOnBuild_label".Translate(), ref settings.editOnBuild, false);
+			Widgets.Label(editDescription, "SaM_Settings_editOnBuild_description".Translate());
+			
+			yOff += editDescription.height + 9;
+
+			Rect pauseCheckbox = new Rect(xPos, yOff, posW, 24);
+			yOff += pauseCheckbox.height + 1;
+			Rect pauseDescription = new Rect(xPos, yOff, posW, 24);
+			
+			Widgets.CheckboxLabeled(pauseCheckbox, "SaM_Settings_pauseOnEdit_label".Translate(), ref settings.pauseGameOnEdit, false);
+			Widgets.Label(pauseDescription, "SaM_Settings_pauseOnEdit_description".Translate());
+		}
+	}
+
+	public class SaM_ModSettings : Verse.ModSettings {
+		//
+		// Fields
+		//
+		public bool editOnBuild = false;
+		public bool pauseGameOnEdit = false;
 
 		//
 		// Constructors
 		//
-		public SaM_Mod(Verse.ModContentPack content) : base(content) {}
+		public SaM_ModSettings() {}
 
 		//
 		// Methods
 		//
+		public override void ExposeData() {
+			Scribe_Values.Look<bool>(ref this.editOnBuild, "edit_on_build", false, false);
+			Scribe_Values.Look<bool>(ref this.pauseGameOnEdit, "pause_game_on_edit", false, false);
+		}
 	}
 
-	public class Base : Building {
+	public class Building_Memorial_Base : Building {
 
         //
         // Static Fields
@@ -28,33 +82,86 @@ namespace SaM {
 		// in minified form this should be 5 but sadly we can't find that out easily
 		// so there will be a stupid scrollbar while in minified form.
 		// you can also not access the full text, but I believe that's not a bad thing
-        private readonly static int MAX_LINES = 6;
-        private readonly static int MAX_LENGTH = 408; // this is valid ONLY for GameFont.Small
-		private readonly static GameFont TEXT_FONT = GameFont.Small;
+        //private static int MAX_LINES = 6;
+        //private static int MAX_LENGTH = 408; // this is valid ONLY for GameFont.Small
+		//private static GameFont TEXT_FONT = GameFont.Small;
 
 		//
 		// Fields
 		//
-		public string text;
+		[Unsaved]
+		private string text;
+
+		//[ThisOneIsSavedTho]
+		private string savedata;
+
+		//
+		// Properties
+		//
+		public string Text {
+			get {
+				return this.text;
+			}
+			set {
+				if(this.text == value) {
+					return;
+				}
+				this.text = value;
+				SaveText();
+			}
+		}
 
 		//
 		// Constructors
 		//
-		public Base() {
-			this.text = "SaM_Placeholder".Translate ();
+		public Building_Memorial_Base() {
 		}
 
 		//
 		// Methods
 		//
+		public void SaveText() {
+			try
+			{
+				Encoding encoding = Encoding.Unicode;
+				savedata = Convert.ToBase64String(
+					CompressUtility.Compress(encoding.GetBytes(text))
+					);
+			}
+			catch(Exception E)
+			{
+				Log.ErrorOnce("Unable to encode memorial data for " + this.ThingID + ": " + E.ToString(), this.ThingID.GetHashCode());
+			}
+		}
+		private void LoadText() {
+			try
+			{
+				Encoding encoding = Encoding.Unicode;
+				char[] base64 = encoding.GetChars(
+					CompressUtility.Decompress( Convert.FromBase64String(savedata) )
+					);
+				text = new string(base64);
+			}
+			catch(Exception E)
+			{
+				Log.ErrorOnce("Unable to decode memorial data for " + this.ThingID + ": " + E.ToString(), this.ThingID.GetHashCode());
+			}
+		}
+
 		public override void ExposeData() {
 			base.ExposeData();
-			Scribe_Values.Look (ref this.text, "text", "ERROR LOADING", false);
+			SaveText();
+			Scribe_Values.Look(ref savedata, "text");
+		}
+
+		public override void PostMapInit() {
+			base.PostMapInit();
+			LoadText();
 		}
 
 		// THIS METHOD HANDLES THE STRING IN THE BOTTOM LEFT
 		public override string GetInspectString() {
-
+			/*
 			// REQUIRED FOR Text.CalcSize TO WORK PROPERLY
 			Text.Font = TEXT_FONT;
 
@@ -62,7 +169,7 @@ namespace SaM {
 
 			foreach(string line in this.text.Split('\n')) {
 				string short_line = "";
-                if (line == "")
+                if (string.IsNullOrEmpty(line))
                 {
                     // lazy fix for an error that occurs when the lower display contains empty lines.
                     // usually that error is most likely valid, but here I believe it's best
@@ -106,13 +213,24 @@ namespace SaM {
 				lines.Add("(...)");
 			}
 			return String.Join("\n", lines.ToArray());
+			*/
+			return this.text;
 		}
 
 		public override TipSignal GetTooltip() {
 			return new TipSignal(this.text, this.thingIDNumber * 152317 /*251235*/, TooltipPriority.Default);
 		}
-	}
 
+		public override void SpawnSetup(Map map, bool respawningAfterLoad) {
+			base.SpawnSetup(map, respawningAfterLoad);
+			if(SaM_Mod.settings?.editOnBuild ?? false) {
+				CameraJumper.TryJumpAndSelect(this);
+				Find.MainTabsRoot.SetCurrentTab(MainButtonDefOf.Inspect);
+				((MainTabWindow_Inspect)MainButtonDefOf.Inspect.TabWindow).OpenTabType = this.GetInspectTabs().OfType<ITab_View>().First().GetType();
+			}
+		}
+	}
+		
 	public class ITab_View : ITab {
 
 		//
@@ -121,6 +239,9 @@ namespace SaM {
 		private bool editing = false;
 		private string text = "";
 		private string tabThingID = null;
+
+		[Unsaved]
+		private bool already_paused = false;
 
 		//
 		// Properties
@@ -146,7 +267,7 @@ namespace SaM {
 		public override void TabUpdate() {
 			base.TabUpdate();
 			if(this.tabThingID != base.SelThing.GetUniqueLoadID()) {
-				this.text = ((Base)base.SelThing).text;
+				this.text = ((Building_Memorial_Base)SelThing).Text;
 				this.tabThingID = base.SelThing.GetUniqueLoadID();
 			}
 		}
@@ -160,21 +281,36 @@ namespace SaM {
 			Rect rectSave = new Rect((this.size.x + 10) / 2, rectButton.y, (rectButton.width - 10) / 2, rectButton.height);
 
 			if(this.editing) {
+				Building_Memorial_Base memorial = (Building_Memorial_Base)base.SelThing;
+
 				this.text = Widgets.TextArea(rectText, this.text);
 
 				if(Widgets.ButtonText(rectCancel, "SaM_TabView_Cancel".Translate())) {
 					this.editing = false;
-					this.text = ((Base)base.SelThing).text;
+					this.text = memorial.Text;
 				}
 				if(Widgets.ButtonText(rectSave, "SaM_TabView_Save".Translate())) {
 					this.editing = false;
-					((Base)base.SelThing).text = this.text;
+					memorial.Text = this.text;
+				}
+				if(!editing) { //state changed
+					if(SaM_Mod.settings.pauseGameOnEdit && Find.TickManager.Paused && !already_paused) {
+						Find.TickManager.TogglePaused();
+					}
+					memorial.SaveText();
 				}
 			} else {
 				Widgets.Label(rectText, this.text);
 
 				if(Widgets.ButtonText(rectButton, "SaM_TabView_Edit".Translate())) {
 					this.editing = true;
+					if(SaM_Mod.settings.pauseGameOnEdit) {
+						if(Find.TickManager.Paused) already_paused = true;
+						else {
+							already_paused = false;
+							Find.TickManager.TogglePaused();
+						}
+					}
 				}
 			}
 		}
